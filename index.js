153dc30e77e809dfec2cb61436eb3da08b6d37a1
Kacper <kacper.figa@o2.pl>
2020-12-18 16:53:26 +0100

    refactor code from oop to functional

--- index.js
+++ index.js
@@ -6,47 +6,95 @@ const spnNext = document.querySelector(".next");
 
 const spnPrev = document.querySelector(".prev");
 
-class Calendar {
-  constructor(spnMonthName, date) {
-    this.spnMonthName = spnMonthName;
+let date = new Date();
 
-    this.date = date;
+months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-    this.months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+getMonth = (date) => date.getMonth();
+
+getYear = (date) => date.getFullYear();
+
+getWeekDay = (date) => date.getDay();
+
+getMonthDays = (date) => {
+  const year = getYear(date);
+  const month = getMonth(date) + 1;
+  const newDate = new Date(year, month, 0);
+  const days = newDate.getDate();
+  return days;
+};
+
+getToday = (date) => date.getDate();
+
+generatePrevMonth = (year, month) => new Date(year, month, 0);
+
+render = (
+  monthIndex,
+  months,
+  daysNumber,
+  currentWeekDay,
+  today,
+  lastDays,
+  lastDayWeek
+) => {
+  spnMonthName.textContent = months[monthIndex];
+
+  const nextDaysToRender = 7 - currentWeekDay;
+
+  const domFragment = document.createDocumentFragment();
+
+  for (let i = lastDayWeek; i > 0; i--) {
+    let dayNumber = lastDays;
+    const day = document.createElement("div");
+    day.className = "calendar__day-prev";
+    day.textContent = dayNumber;
+    domFragment.appendChild(day);
+    dayNumber = -1;
   }
 
-  getMonth = (date) => date.getMonth();
+  for (let i = 1; i <= daysNumber; i++) {
+    const day = document.createElement("div");
+    if (i == today) {
+      day.className = "calendar__day-today";
+    } else day.classList.add("calendar__day");
 
-  getYear = (date) => date.getFullYear();
+    day.textContent = i;
+    domFragment.appendChild(day);
+  }
 
-  getWeekDay = (date) => date.getDay();
+  for (let i = 1; i <= nextDaysToRender; i++) {
+    const day = document.createElement("div");
+    day.className = "calendar__day-next";
+    day.textContent = i;
+    domFragment.appendChild(day);
+  }
 
-  getMonthDays = (date) => {
-    const year = this.getYear(date);
-    const month = this.getMonth(date) + 1;
-    const newDate = new Date(year, month, 0);
-    const days = newDate.getDate();
-    return days;
-  };
+  calendarBottomBar.appendChild(domFragment);
+};
 
-  getToday = (date) => date.getDate();
-
-  generatePrevMonth = (year, month) => new Date(year, month, 0);
-
-  render = (
+init = () => {
+  const daysNumber = getMonthDays(date);
+  const monthIndex = getMonth(date);
+  const year = getYear(date);
+  const currentWeekDay = getWeekDay(date);
+  const today = getToday(date);
+  const prevMonth = generatePrevMonth(year, monthIndex);
+  const lastDays = getMonthDays(prevMonth);
+  const lastDayWeek = getWeekDay(prevMonth);
+  render(
     monthIndex,
     months,
     daysNumber,
@@ -54,66 +102,7 @@ class Calendar {
     today,
     lastDays,
     lastDayWeek
-  ) => {
-    console.log(lastDayWeek);
+  );
+};
 
-    this.spnMonthName.textContent = months[monthIndex];
-
-    const nextDaysToRender = 7 - currentWeekDay;
-
-    const domFragment = document.createDocumentFragment();
-
-    for (let i = lastDayWeek; i > 0; i--) {
-      let dayNumber = lastDays;
-      const day = document.createElement("div");
-      day.className = "calendar__day-prev";
-      day.textContent = dayNumber;
-      domFragment.appendChild(day);
-      dayNumber = -1;
-    }
-
-    for (let i = 1; i <= daysNumber; i++) {
-      const day = document.createElement("div");
-      if (i == today) {
-        day.className = "calendar__day-today";
-      } else day.classList.add("calendar__day");
-
-      day.textContent = i;
-      domFragment.appendChild(day);
-    }
-
-    for (let i = 1; i <= nextDaysToRender; i++) {
-      const day = document.createElement("div");
-      day.className = "calendar__day-next";
-      day.textContent = i;
-      domFragment.appendChild(day);
-    }
-
-    calendarBottomBar.appendChild(domFragment);
-  };
-
-  init = () => {
-    const daysNumber = this.getMonthDays(this.date);
-    const monthIndex = this.getMonth(this.date);
-    const year = this.getYear(this.date);
-    const currentWeekDay = this.getWeekDay(this.date);
-    const today = this.getToday(date);
-    const prevMonth = this.generatePrevMonth(year, monthIndex);
-    const lastDays = this.getMonthDays(prevMonth);
-    const lastDayWeek = this.getWeekDay(prevMonth);
-    this.render(
-      monthIndex,
-      this.months,
-      daysNumber,
-      currentWeekDay,
-      today,
-      lastDays,
-      lastDayWeek
-    );
-  };
-}
-
-const date = new Date();
-const calendar = new Calendar(spnMonthName, date);
-
-calendar.init();
+init();
